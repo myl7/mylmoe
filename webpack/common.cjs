a238fc6cfc1670b6ce373099202c07b2241b5dad
myl7 <myl.ustc@gmail.com>
2020-10-04 04:29:47 +0800

    Update webpack config.

--- webpack/common.cjs
+++ webpack/common.cjs
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
--- webpack/dev.cjs
+++ webpack/dev.cjs
@@ -1,5 +1,5 @@
 const {merge} = require('webpack-merge')
-const common = require('./webpack.common.cjs')
+const common = require('./common.cjs')
 
 module.exports = merge(common, {
   mode: 'development',
