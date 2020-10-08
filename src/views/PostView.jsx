70d23ade51f7ebc9b42eac6b155b353bea8bb4a4
myl7 <myl.ustc@gmail.com>
2020-10-09 07:58:41 +0800

    Use dynamic import for marked.

--- src/views/PostView.jsx
+++ src/views/PostView.jsx
@@ -3,25 +3,9 @@ import {Card, CardContent} from '@material-ui/core'
 import {useParams} from 'react-router-dom'
 import bash from 'highlight.js/lib/languages/bash'
 import python from 'highlight.js/lib/languages/python'
-import marked from 'marked'
+import vim from 'highlight.js/lib/languages/vim'
+import json from 'highlight.js/lib/languages/json'
 import posts from '../posts'
-
-const renderer = new marked.Renderer()
-const linkRenderer = renderer.link
-renderer.link = (href, title, text) => {
-  let html = linkRenderer.call(renderer, href, title, text)
-  if (!href.startsWith('/')) {
-    html = html.replace(/^<a /, '<a class="MuiLink-underlineHover" target="_blank" rel="noopener" ')
-  }
-  return html
-}
-const codeRenderer = renderer.code
-renderer.code = (code, infostring, escaped) => {
-  let html = codeRenderer.call(renderer, code, infostring, escaped)
-  html = html.replace(/<code>/, '<code class="plaintext">')
-  return html
-}
-marked.setOptions({renderer})
 
 export default () => {
   const {slug} = useParams()
@@ -30,13 +14,39 @@ export default () => {
   useEffect(() => {
     fetch(posts.find((p, _i) => p.slug === slug).url).then((resp) => {
       resp.text().then((content) => {
-        setBody(marked(content))
+        setBody(content)
 
-        import(/* webpackChunkName: "highlight" */ 'highlight.js/lib/core').then((hljs) => {
-          hljs.registerLanguage('bash', bash)
-          hljs.registerLanguage('python', python)
-          hljs.initHighlighting()
-        }).catch(e => throw e)
+        const markedPromise = import(/* webpackChunkName: "marked" */ 'marked')
+        const highlightJsPromise = import(/* webpackChunkName: "highlight.js" */ 'highlight.js/lib/core')
+
+        markedPromise.then(({default: marked}) => {
+          const renderer = new marked.Renderer()
+          const linkRenderer = renderer.link
+          renderer.link = (href, title, text) => {
+            let html = linkRenderer.call(renderer, href, title, text)
+            if (!href.startsWith('/')) {
+              html = html.replace(/^<a /, '<a class="MuiLink-underlineHover" target="_blank" rel="noopener" ')
+            }
+            return html
+          }
+          const codeRenderer = renderer.code
+          renderer.code = (code, infostring, escaped) => {
+            let html = codeRenderer.call(renderer, code, infostring, escaped)
+            html = html.replace(/<code>/, '<code class="plaintext">')
+            return html
+          }
+          marked.setOptions({renderer})
+
+          setBody(marked(content))
+
+          highlightJsPromise.then(hljs => {
+            hljs.registerLanguage('bash', bash)
+            hljs.registerLanguage('python', python)
+            hljs.registerLanguage('vim', vim)
+            hljs.registerLanguage('json', json)
+            hljs.initHighlighting()
+          }).catch(e => throw e)
+        })
       })
     }).catch((e) => throw e)
   }, [setBody])
