504796a5be6f14f9c8069e8778cc76821948576a
myl7 <myl.ustc@gmail.com>
2020-10-09 17:50:40 +0800

    Separate get post meta func.

--- scripts/buildPostMeta.js
+++ scripts/buildPostMeta.js
@@ -1,32 +1,9 @@
 import fs from 'fs'
-import glob from 'glob'
-import YAML from 'js-yaml'
+import getPostMeta from './getPostMeta.js'
 
-const postPattern = './src/posts/*.md'
 const postMetaPath = './src/posts.js'
 
-const postPaths = glob.sync(postPattern)
-
-let postMeta = []
-postPaths.forEach((postPath) => {
-  let content = fs.readFileSync(postPath).toString()
-  const header = content.substring(0, content.indexOf('---'))
-  let meta = YAML.safeLoad(header)
-
-  const setIfNot = (obj, k, v) => {
-    if (!obj[k]) obj[k] = v
-  }
-  const slug = postPath.substring(postPath.lastIndexOf('/') + 1, postPath.lastIndexOf('.'))
-  setIfNot(meta, 'slug', slug)
-  const date = slug.substring(0, 10)
-  setIfNot(meta, 'date', date)
-
-  meta.url = 'url' + meta.id
-
-  postMeta.push(meta)
-})
-
-postMeta.sort((a, b) => -a.date.localeCompare(b.date))
+const postMeta = getPostMeta()
 let postExport = 'export default ' + JSON.stringify(postMeta, null, 2)
 postExport = postExport.replace(/"url(\d+)"/g, (_m, id) => `url${id}`)
 postExport = postExport.replace(/'/g, '"')
---
+++ scripts/getPostMeta.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import glob from 'glob'
+import YAML from 'js-yaml'
+
+const postPattern = './src/posts/*.md'
+
+export default () => {
+  const postPaths = glob.sync(postPattern)
+
+  let postMeta = []
+  postPaths.forEach((postPath) => {
+    let content = fs.readFileSync(postPath).toString()
+    const header = content.substring(0, content.indexOf('---'))
+    let meta = YAML.safeLoad(header)
+
+    const setIfNot = (obj, k, v) => {
+      if (!obj[k]) obj[k] = v
+    }
+    const slug = postPath.substring(postPath.lastIndexOf('/') + 1, postPath.lastIndexOf('.'))
+    setIfNot(meta, 'slug', slug)
+    const date = slug.substring(0, 10)
+    setIfNot(meta, 'date', date)
+
+    meta.url = 'url' + meta.id
+
+    postMeta.push(meta)
+  })
+
+  postMeta.sort((a, b) => -a.date.localeCompare(b.date))
+  return postMeta
+}
