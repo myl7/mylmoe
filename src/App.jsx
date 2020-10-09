ba5d2b1dab8bea899a7cf43863fcba4803009deb
myl7 <myl.ustc@gmail.com>
2020-10-09 22:03:16 +0800

    Add RSS icon to app bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {AppBar, Toolbar, IconButton, Link, Typography} from '@material-ui/core'
-import {Home as HomeIcon, GitHub as GitHubIcon, Mail as MailIcon} from '@material-ui/icons'
+import {Home as HomeIcon, GitHub as GitHubIcon, Mail as MailIcon, RssFeed as RssFeedIcon} from '@material-ui/icons'
 import {Link as RouterLink} from 'react-router-dom'
 import IndexView from './views/IndexView'
 import PostView from './views/PostView'
@@ -14,6 +14,8 @@ export default () => {
           <Toolbar>
             <IconButton edge={'start'} color={'inherit'} children={<HomeIcon />} component={RouterLink} to={'/'} />
             <Typography variant={'h6'} style={{flexGrow: 1}}>mlblog</Typography>
+            <IconButton children={<RssFeedIcon color={'action'} />}
+                        component={Link} href={'https://myl.moe/rss.xml'} target={'_blank'} />
             <IconButton children={<MailIcon color={'action'} />}
                         component={Link} href={'mailto:myl.ustc@gmail.com'} target={'_blank'} />
             <IconButton edge={'end'} children={<GitHubIcon color={'action'} />}
