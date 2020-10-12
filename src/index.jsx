bde85112623ee5d1cb9d015e55878bde7e82861c
myl7 <myl.ustc@gmail.com>
2020-10-12 08:10:24 +0800

    Separate css into index.css.

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './rss.xml'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack/common.cjs
+++ webpack/common.cjs
@@ -17,6 +17,10 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.md$/,
         use: [
           {
