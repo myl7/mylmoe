7c11301bfd1a411fe8e3805de7bf28b47a1f54a6
myl7 <myl.ustc@gmail.com>
2020-10-14 03:05:28 +0800

    Fix wasm-opt to reduce size.
    Update error return.

--- workers/arcaea_prober_wrapper/src/lib.rs
+++ workers/arcaea_prober_wrapper/src/lib.rs
@@ -12,10 +12,10 @@ pub fn brotli_dec(buf: Box<[u8]>) -> Result<String, JsValue> {
     let mut out = Vec::<u8>::new();
     match BrotliDecompress(&mut buf.as_ref(), &mut out) {
         Ok(_) => (),
-        Err(e) => return Err(JsValue::from_str(&format!("{}", e))),
+        Err(_) => return Err(JsValue::from_str("brotli dec failed")),
     }
     match String::from_utf8(out) {
         Ok(s) => Ok(s),
-        Err(e) => return Err(JsValue::from_str(&format!("{}", e))),
+        Err(_) => return Err(JsValue::from_str("utf-8 dec failed")),
     }
 }
