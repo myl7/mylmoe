f974da6f48db9e2d07d9c77215de01c0f9019409
myl7 <myl.ustc@gmail.com>
2020-10-15 23:41:29 +0800

    Remove useless CF worker.

--- workers/brotli_dec/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf ./dist
-./node_modules/.bin/esbuild --target=chrome86 --format=esm --outdir=./dist --bundle index.js
-cp ./node_modules/brotli_dec_wasm/brotli_dec_wasm_bg.wasm ./dist/
