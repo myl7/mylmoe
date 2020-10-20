d9b90e4c449f8a72f5c63982a273b9aff00bc79a
myl7 <myl.ustc@gmail.com>
2020-10-20 13:34:39 +0800

    Setup webpack gzip compress.

--- webpack/prod.cjs
+++ webpack/prod.cjs
@@ -1,6 +1,13 @@
 const {merge} = require('webpack-merge')
 const common = require('./common.cjs')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CompressionPlugin({
+      test: /\.js$/,
+      deleteOriginalAssets: true
+    })
+  ]
 })
