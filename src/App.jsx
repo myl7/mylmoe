e4839a8f6c22aacc1861616103bd9715710b69ed
myl7 <myl.ustc@gmail.com>
2020-11-19 05:21:44 +0800

    Separate app and router. Rename view to page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {MuiThemeProvider} from '@material-ui/core'
-import IndexView from './views/IndexView'
-import PostView from './views/PostView'
-import ArcaeaView from './views/ArcaeaView'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import theme from './utils/theme'
+import Router from './Router'
 
 export default () => {
   return (
     <div>
-      <Router>
-        <MuiThemeProvider theme={theme}>
-          <Header />
-          <Switch>
-            <Route exact path={'/'} children={<IndexView />} />
-            <Route path={'/posts/:slug'} children={<PostView />} />
-            <Route path={'/pages/arcaea'} children={<ArcaeaView />} />
-          </Switch>
-          <Footer />
-        </MuiThemeProvider>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router />
+      </MuiThemeProvider>
     </div>
   )
 }
---
+++ src/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import IndexPage from './pages/IndexPage'
+import PostPage from './pages/PostPage'
+import ArcaeaPage from './pages/ArcaeaPage'
+
+export default () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={'/'} children={<IndexPage />} />
+        <Route path={'/posts/:slug'} children={<PostPage />} />
+        <Route path={'/pages/arcaea'} children={<ArcaeaPage />} />
+      </Switch>
+    </Router>
+  )
+}
