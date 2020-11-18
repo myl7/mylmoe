6c3bbb0e8600152fdb60a7de1fb474f7be424bd9
myl7 <myl.ustc@gmail.com>
2020-11-19 05:09:15 +0800

    Separate dayjs utils out.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
 import App from './App'
-
-dayjs.extend(utc)
 
 ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/utils/dayjs.js
@@ -0,0 +1,8 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
+const format = (d, fs) => dayjs.utc(d).local().format(fs)
+export const formatDatetime = d => format(d, 'YYYY-MM-DD HH:mm:ss')
+export const formatDate = d => format(d, 'YYYY-MM-DD')
