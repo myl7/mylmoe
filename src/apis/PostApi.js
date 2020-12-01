f739eee1bf30cf334fad7e0beb73269f7c9c9c8b
myl7 <myl.ustc@gmail.com>
2020-12-01 21:45:55 +0800

    Fix api resp json date parsing.

--- src/apis/PostApi.js
+++ src/apis/PostApi.js
@@ -1,5 +1,7 @@
 import * as postApiMock from './postApi.mock'
 
+const normalDate = dateObj => `${dateObj.year}-${dateObj.month.padStart(2, '0')}-${dateObj.day.padStart(2, '0')}`
+
 export default class PostApi {
   async posts() {
     if (process.env.NODE_ENV === 'development') {
@@ -9,7 +11,14 @@ export default class PostApi {
     const res = await fetch('' +
       'https://mlapis.azure-api.net/mlpost-public/GetPost?subscription-key=504fd063894b4e1aaa11d6f38a66820c'
     )
-    return res.status === 200 ? await res.json() : undefined
+    if (res.status !== 200) {
+      return undefined
+    }
+
+    let body = await res.json()
+    body.pubDate = normalDate(body.pubDate)
+    body.updDate = normalDate(body.updDate)
+    return body
   }
 
   async post(slug) {
@@ -20,6 +29,13 @@ export default class PostApi {
     const res = await fetch(
       `'https://mlapis.azure-api.net/mlpost-public/GetPost?slug=${slug}&subscription-key=504fd063894b4e1aaa11d6f38a66820c'`
     )
-    return res.status === 200 ? await res.json() : undefined
+    if (res.status !== 200) {
+      return undefined
+    }
+
+    let body = await res.json()
+    body.pubDate = normalDate(body.pubDate)
+    body.updDate = normalDate(body.updDate)
+    return body
   }
 }
