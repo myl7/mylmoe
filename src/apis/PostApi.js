6c0b625f75592221d03a06582cec98f19d863d48
myl7 <myl.ustc@gmail.com>
2020-12-05 22:04:52 +0800

    Update api.

--- src/apis/PostApi.js
+++ src/apis/PostApi.js
@@ -1,10 +1,5 @@
 import * as postApiMock from './postApi.mock'
 
-const normalDate = dateObj => `\
-${dateObj.year}-\
-${dateObj.month.toString().padStart(2, '0')}-\
-${dateObj.day.toString().padStart(2, '0')}`
-
 export default class PostApi {
   async posts() {
     if (process.env.NODE_ENV === 'development') {
@@ -14,18 +9,7 @@ export default class PostApi {
     const res = await fetch('' +
       'https://mlapis.azure-api.net/mlpost-public/GetPost?subscription-key=504fd063894b4e1aaa11d6f38a66820c'
     )
-    if (res.status !== 200) {
-      return undefined
-    }
-
-    let posts = await res.json()
-    for (let post of posts) {
-      if (post !== undefined) {
-        post.pubDate = normalDate(post.pubDate)
-        post.updDate = normalDate(post.updDate)
-      }
-    }
-    return posts
+    return res.status === 200 ? await res.json() : undefined
   }
 
   async post(slug) {
@@ -36,13 +20,6 @@ export default class PostApi {
     const res = await fetch(
       `https://mlapis.azure-api.net/mlpost-public/GetPost?slug=${slug}&subscription-key=504fd063894b4e1aaa11d6f38a66820c`
     )
-    if (res.status !== 200) {
-      return undefined
-    }
-
-    let post = await res.json()
-    post.pubDate = normalDate(post.pubDate)
-    post.updDate = normalDate(post.updDate)
-    return post
+    return res.status === 200 ? await res.json() : undefined
   }
 }
