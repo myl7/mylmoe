9ea2e0dfe660376f01676727d34dc35f0040f119
myl7 <myl.ustc@gmail.com>
2020-12-22 22:38:07 +0800

    Fix post api cors.

--- apis/post/index.js
+++ apis/post/index.js
@@ -2,20 +2,28 @@ addEventListener('fetch', e => {
   e.respondWith(handleReq(e.request))
 })
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*'
+}
+
 const handleReq = async (req) => {
-  const sel = (await req.json()).slug
+  let sel = ''
+  if (req.method === 'POST') {
+    sel = (await req.json()).slug
+  }
 
   if (sel) {
     const post = await MylmoePostNS.get(sel)
 
     if (post === null) {
-      return new Response('Post not found', {status: 404})
+      return new Response('Post not found', {status: 404, headers: headers})
     } else {
-      return new Response(post, {headers: {'content-type': 'application/json'}})
+      return new Response(post, {headers: {'content-type': 'application/json', ...headers}})
     }
   } else {
     const list = await MylmoePostNS.get('list')
 
-    return new Response(list, {headers: {'content-type': 'application/json'}})
+    return new Response(list, {headers: {'content-type': 'application/json', ...headers}})
   }
 }
