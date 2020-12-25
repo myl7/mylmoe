242f2feb761eed06e4f1d8982d367a78af21bec5
myl7 <myl.ustc@gmail.com>
2020-12-25 20:14:28 +0800

    Fix arcaea api with mock.

--- src/apis/ArcaeaApi.js
+++ src/apis/ArcaeaApi.js
@@ -1,4 +1,4 @@
-import data from './ArcaeaApiData.mock.json'
+const subKey = '7e1826c345a941f8a6e928b95626cf5c'
 
 export default class ArcaeaApi {
   async data() {
@@ -6,7 +6,16 @@ export default class ArcaeaApi {
       return data
     }
 
-    const res = await fetch('/api/arcaea')
-    return res.status === 200 ? await res.json() : undefined
+    const url = new URL('https://mlapis.azure-api.net/mylmoe-arcaea/Get')
+    url.search = new URLSearchParams({'subscription-key': subKey}).toString()
+    const res = await fetch(url.toString())
+    if (res.status !== 200) {
+      return undefined
+    }
+
+    const raw = await res.blob()
+    const dec = new DecompressionStream('gzip')
+    const out = raw.stream().pipeThrough(dec)
+    return await new Response(out).json()
   }
 }
