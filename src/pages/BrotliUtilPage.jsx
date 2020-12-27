b8cc9f8646a842dc4fac007b64551b38e3c8eb2d
myl7 <myl.ustc@gmail.com>
2020-12-28 03:28:00 +0800

    Add titles for new pages

--- src/pages/BrotliUtilPage.jsx
+++ src/pages/BrotliUtilPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {CardContent, makeStyles, Divider} from '@material-ui/core'
+import {CardContent, makeStyles, Divider, Typography} from '@material-ui/core'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ContentCard from '../components/ContentCard'
+import WideDivider from '../components/WideDivider'
 
 const useStyles = makeStyles(theme => ({}))
 
@@ -15,6 +16,10 @@ export default () => {
       <Divider />
       <ContentCard>
         <CardContent>
+          <Typography variant={'h4'}>
+            Brotli
+          </Typography>
+          <WideDivider />
           !!WIP!!
         </CardContent>
       </ContentCard>
