319d9cfbc561527e5c7d8ddc56be2113c2ac2458
myl7 <myl.ustc@gmail.com>
2020-12-27 19:51:59 +0800

    Add friend page

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import PostPage from './pages/PostPage'
 import ArcaeaPage from './pages/ArcaeaPage'
+import FriendPage from './pages/FriendPage'
 
 export default () => {
   return (
@@ -11,6 +12,7 @@ export default () => {
         <Route exact path={'/'} children={<IndexPage />} />
         <Route path={'/posts/:slug'} children={<PostPage />} />
         <Route path={'/pages/arcaea'} children={<ArcaeaPage />} />
+        <Route path={'/pages/friends'} children={<FriendPage />} />
       </Switch>
     </Router>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,54 @@
 import React from 'react'
-import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core'
+import {AppBar, Button, IconButton, Toolbar, Typography, makeStyles} from '@material-ui/core'
 import {GitHub as GitHubIcon, Home as HomeIcon, Mail as MailIcon, RssFeed as RssFeedIcon} from '@material-ui/icons'
 import ExternalLink from './ExternalLink'
 import RouterLink from './RouterLink'
 import MenuButton from './MenuButton'
 
+const useStyles = makeStyles({
+  toolbar: {
+    paddingLeft: 12,
+    paddingRight: 12
+  },
+  headerButtonFirst: {
+    marginLeft: '2em',
+    '&:hover': {
+      textDecoration: 'none'
+    }
+  },
+  headerButton: {
+    marginLeft: '1em',
+    '&:hover': {
+      textDecoration: 'none'
+    }
+  },
+  flexDivider: {
+    flexGrow: 1
+  }
+})
+
 export default () => {
+  const classes = useStyles()
+
   return (
     <AppBar position={'static'} color={'secondary'}>
-      <Toolbar style={{paddingLeft: 12, paddingRight: 12}}>
+      <Toolbar className={classes.toolbar}>
         <IconButton component={RouterLink} to={'/'}>
           <HomeIcon color={'primary'} />
         </IconButton>
         <Typography variant={'h6'}>mylmoe</Typography>
 
-        <MenuButton id={'header-page-menu'} style={{marginLeft: '2em'}} items={[
+        <MenuButton id={'header-page-menu'} className={classes.headerButtonFirst} items={[
           {component: RouterLink, to: '/pages/arcaea', children: 'Arcaea'}
         ]}>
           <Typography variant={'subtitle1'}>Pages</Typography>
         </MenuButton>
+        <Button className={classes.headerButton} variant={'outlined'} color={'primary'} component={RouterLink}
+                to={'/pages/friends'}>
+          <Typography variant={'subtitle1'}>Friends</Typography>
+        </Button>
 
-        <div style={{flexGrow: 1}} />
+        <div className={classes.flexDivider} />
         <IconButton component={ExternalLink} href={'https://myl.moe/rss'}>
           <RssFeedIcon color={'primary'} />
         </IconButton>
