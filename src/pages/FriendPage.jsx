f343649418e1167f6e10b4dff52c5ecba5e76bd6
myl7 <myl.ustc@gmail.com>
2021-01-01 21:24:02 +0800

    Add friend page structure

--- src/pages/FriendPage.jsx
+++ src/pages/FriendPage.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-import {CardContent, makeStyles, Divider, Typography} from '@material-ui/core'
+import {
+  CardContent,
+  Divider,
+  Typography,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Table,
+  Card,
+  makeStyles
+} from '@material-ui/core'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ContentCard from '../components/ContentCard'
+import ExternalLink from '../components/ExternalLink'
 import WideDivider from '../components/WideDivider'
 
-const useStyles = makeStyles(theme => ({}))
+const friends = [
+  {
+    title: 'Totoro’s Blog',
+    url: 'https://yyw.moe',
+    rssUrl: 'https://yyw.moe/atom.xml'
+  }
+]
+
+const relativePath = (root, url) => {
+  if (url.substring(0, root.length) === root) {
+    return url.substring(root.length)
+  } else {
+    return url
+  }
+}
+
+const useStyles = makeStyles(theme => ({
+  content: {
+    backgroundColor: theme.palette.background.main
+  },
+  table: {
+    backgroundColor: theme.palette.secondary.main
+  }
+}))
 
 export default () => {
   const classes = useStyles()
@@ -14,13 +50,44 @@ export default () => {
     <div>
       <Header />
       <Divider />
-      <ContentCard>
+      <ContentCard className={classes.content}>
         <CardContent>
           <Typography variant={'h4'}>
             Friends
           </Typography>
           <WideDivider />
-          !!WIP!!
+          <TableContainer className={classes.table} component={Card}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Title</TableCell>
+                  <TableCell>URL</TableCell>
+                  <TableCell>RSS URL</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {friends.map(({title, url, rssUrl}) => (
+                  <TableRow key={url}>
+                    <TableCell component={'th'} scope={'row'}>
+                      <Typography variant={'subtitle1'} component={ExternalLink} href={url}>
+                        {title}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant={'subtitle1'} component={ExternalLink} href={url}>
+                        {url}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant={'subtitle1'} component={ExternalLink} href={rssUrl}>
+                        {relativePath(url, rssUrl)}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </CardContent>
       </ContentCard>
       <Footer />
