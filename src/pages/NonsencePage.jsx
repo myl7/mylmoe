b81152f5e5d480b48b2a5efef87d03a9eeaf5cab
myl7 <myl.ustc@gmail.com>
2021-01-02 16:00:31 +0800

    Add md support for ideas

--- src/pages/NonsencePage.jsx
+++ src/pages/NonsencePage.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, Fragment, useState} from 'react'
 import {CardContent, Typography, Grid, makeStyles, Divider} from '@material-ui/core'
+import Markdown from 'markdown-to-jsx'
 import {formatDatetime} from '../utils/dayjs'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ContentCard from '../components/ContentCard'
 import IdeaApi from '../apis/IdeaApi'
+import {md2jsxOptions} from '../utils/md2jsx'
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -50,9 +52,9 @@ export default () => {
                       </Typography>
                     </Grid>
                   </Grid>
-                  <Typography variant={'subtitle1'}>
+                  <Markdown options={md2jsxOptions}>
                     {idea.body}
-                  </Typography>
+                  </Markdown>
                 </CardContent>
               </ContentCard>
               <Divider variant={'middle'} className={classes.ideaItemDivider} />
