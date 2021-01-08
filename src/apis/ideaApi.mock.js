eceeaf22f1496a585cfe69cf4727ead6556dd167
myl7 <myl@myl.moe>
2021-01-08 16:52:56 +0800

    Add markdown fold

--- src/apis/ideaApi.mock.js
+++ src/apis/ideaApi.mock.js
@@ -2,12 +2,51 @@ export const ideas1 = {
   ideas: [
     {
       title: 'B 站和 QQ 音乐的 2020 年度报告',
-      body: '虽然是挺屑的两个 app，不过我还是在这两个 app 上消磨的时间比较多。\n这样算是展示一下兴趣？\n\nB 站：\n\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/1.jpg" alt="bilibili-1" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/2.jpg" alt="bilibili-2" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/3.jpg" alt="bilibili-3" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/4.jpg" alt="bilibili-4" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/5.jpg" alt="bilibili-5" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/6.jpg" alt="bilibili-6" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/7.jpg" alt="bilibili-7" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/8.jpg" alt="bilibili-8" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/9.jpg" alt="bilibili-9" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/10.jpg" alt="bilibili-10" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/11.jpg" alt="bilibili-11" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/12.jpg" alt="bilibili-12" width="300px" />\n\nQQ 音乐：\n\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/1.jpg" alt="qq-music-1" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/2.jpg" alt="qq-music-2" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/3.jpg" alt="qq-music-3" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/4.jpg" alt="qq-music-4" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/5.jpg" alt="qq-music-5" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/6.jpg" alt="qq-music-6" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/7.jpg" alt="qq-music-7" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/8.jpg" alt="qq-music-8" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/9.jpg" alt="qq-music-9" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/10.jpg" alt="qq-music-10" width="300px" />\n<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/11.jpg" alt="qq-music-11" width="300px" />\n',
+      body: `\
+虽然是挺屑的两个 app，不过我还是在这两个 app 上消磨的时间比较多。
+这样算是展示一下兴趣？
+
+B 站：
+
+<Fold summary="B 站年度报告" detail={
+  <div>
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/1.jpg" alt="bilibili-1" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/2.jpg" alt="bilibili-2" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/3.jpg" alt="bilibili-3" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/4.jpg" alt="bilibili-4" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/5.jpg" alt="bilibili-5" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/6.jpg" alt="bilibili-6" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/7.jpg" alt="bilibili-7" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/8.jpg" alt="bilibili-8" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/9.jpg" alt="bilibili-9" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/10.jpg" alt="bilibili-10" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/11.jpg" alt="bilibili-11" width="300px" />
+    <img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/bilibili/12.jpg" alt="bilibili-12" width="300px" />
+  </div>
+} />
+
+QQ 音乐：
+
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/1.jpg" alt="qq-music-1" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/2.jpg" alt="qq-music-2" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/3.jpg" alt="qq-music-3" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/4.jpg" alt="qq-music-4" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/5.jpg" alt="qq-music-5" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/6.jpg" alt="qq-music-6" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/7.jpg" alt="qq-music-7" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/8.jpg" alt="qq-music-8" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/9.jpg" alt="qq-music-9" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/10.jpg" alt="qq-music-10" width="300px" />
+<img src="https://cdn.jsdelivr.net/gh/myl7/mylmoe-images@goshujin-sama/annual-reports/2020/qq-music/11.jpg" alt="qq-music-11" width="300px" />
+`,
       pubTime: '2021-01-05T19:04:33'
     },
     {
       title: 'nonsence 基础设施',
-      body: '把 nonsence 添加 idea 的面板加上了，现在发 nonsence 应该比较方便了。\n还有一些小问题，比如输入框打不了中文，会自动 enter 把字上屏……之后慢慢修吧。\n',
+      body: `\
+  把 nonsence 添加 idea 的面板加上了，现在发 nonsence 应该比较方便了。
+  还有一些小问题，比如输入框打不了中文，会自动 enter 把字上屏……之后慢慢修吧。
+`,
       pubTime: '2021-01-05T08:45:28'
     },
     {
--- src/utils/md2jsx.jsx
+++ src/utils/md2jsx.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Accordion, AccordionDetails, AccordionSummary} from '@material-ui/core'
+import {ExpandMore as ExpandMoreIcon} from '@material-ui/icons'
+import Markdown from 'markdown-to-jsx'
 import ExternalLink from '../components/ExternalLink'
 import RouterLink from '../components/RouterLink'
 
-const AutoA = (props) => {
+const AutoA = props => {
   const {href, className, ...others} = props
 
   if (href.startsWith('/')) {
@@ -12,8 +15,30 @@ const AutoA = (props) => {
   }
 }
 
+const Fold = props => {
+  const {summary, detail, summaryProps, ...others} = props
+
+  console.log(detail)
+
+  return (
+    <Accordion {...others}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} {...summaryProps}>
+        <Markdown options={md2jsxOptions}>{
+          summary && summary[0] === '{' ? summary.substring(1, summary.length - 1) : summary
+        }</Markdown>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Markdown options={md2jsxOptions}>{
+          detail && detail[0] === '{' ? detail.substring(1, detail.length - 1) : detail
+        }</Markdown>
+      </AccordionDetails>
+    </Accordion>
+  )
+}
+
 export const md2jsxOptions = {
   overrides: {
-    a: AutoA
+    a: AutoA,
+    Fold: {component: Fold}
   }
 }
