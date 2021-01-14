a701d577a2b41e248105578ad2bc2e3ff031ceb6
myl7 <myl@myl.moe>
2021-01-14 16:45:04 +0800

    Fix friend page

--- src/pages/FriendPage.jsx
+++ src/pages/FriendPage.jsx
@@ -1,23 +1,9 @@
 import React from 'react'
 import {
-  CardContent,
-  Divider,
-  Typography,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Table,
-  Card,
-  makeStyles
+  Typography, TableContainer, TableHead, TableRow, TableCell, TableBody, Table, Card
 } from '@material-ui/core'
-import {Helmet} from 'react-helmet'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import ContentCard from '../components/ContentCard'
 import ExternalLink from '../components/ExternalLink'
-import WideDivider from '../components/WideDivider'
+import BodyPage from './templates/BodyPage'
 
 const friends = [
   {
@@ -78,84 +64,58 @@ const relativePath = (root, url) => {
   }
 }
 
-const useStyles = makeStyles(theme => ({
-  content: {
-    backgroundColor: theme.palette.background.main
-  },
-  table: {
-    backgroundColor: theme.palette.secondary.main
-  }
-}))
-
 export default () => {
-  const classes = useStyles()
-
   return (
-    <div>
-      <Helmet>
-        <title>Friends | mylmoe</title>
-        <meta name={'description'} content={'mylmoe friend page containing friend websites'} />
-        <link rel="canonical" href="https://myl.moe/pages/friends" />
-      </Helmet>
-      <Header />
-      <Divider />
-      <ContentCard className={classes.content}>
-        <CardContent>
-          <Typography variant={'h4'} component={'h1'}>
-            Friends
-          </Typography>
-          <WideDivider />
-          <TableContainer className={classes.table} component={Card}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Title</TableCell>
-                  <TableCell>URL</TableCell>
-                  <TableCell>Author</TableCell>
-                  <TableCell>RSS</TableCell>
-                  <TableCell>GitHub</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {friends.map(({title, url, author, about, rss, github}) => (
-                  <TableRow key={url}>
-                    <TableCell component={'th'} scope={'row'}>
-                      <Typography variant={'subtitle1'} component={ExternalLink} href={url}>
-                        {title}
+    <BodyPage title={'Friends'} description={'myl7\'s friends who have a blog'} path={'/pages/friends'}>
+      <TableContainer component={Card}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>URL</TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell>RSS</TableCell>
+              <TableCell>GitHub</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {friends.map(({title, url, author, about, rss, github}) => (
+              <TableRow key={url}>
+                <TableCell component={'th'} scope={'row'}>
+                  <Typography variant={'subtitle1'} color={'textPrimary'} component={ExternalLink} href={url}>
+                    {title}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant={'subtitle1'} color={'textPrimary'} component={ExternalLink} href={url}>
+                    {url}
+                  </Typography>
+                </TableCell>
+                <TableCell component={'th'} scope={'row'}>
+                  <Typography variant={'subtitle1'} color={'textPrimary'} component={ExternalLink} href={about}>
+                    {author}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  {
+                    rss ? (
+                      <Typography variant={'subtitle1'} color={'textPrimary'} component={ExternalLink} href={rss}>
+                        {relativePath(url, rss)}
                       </Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant={'subtitle1'} component={ExternalLink} href={url}>
-                        {url}
-                      </Typography>
-                    </TableCell>
-                    <TableCell component={'th'} scope={'row'}>
-                      <Typography variant={'subtitle1'} component={ExternalLink} href={about}>
-                        {author}
-                      </Typography>
-                    </TableCell>
-                    <TableCell>
-                      {
-                        rss ? (
-                          <Typography variant={'subtitle1'} component={ExternalLink} href={rss}>
-                            {relativePath(url, rss)}
-                          </Typography>
-                        ) : ''
-                      }
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant={'subtitle1'} component={ExternalLink} href={'https://github.com/' + github}>
-                        {github}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
-      </ContentCard>
-      <Footer />
-    </div>
+                    ) : ''
+                  }
+                </TableCell>
+                <TableCell>
+                  <Typography variant={'subtitle1'} color={'textPrimary'} component={ExternalLink}
+                              href={'https://github.com/' + github}>
+                    {github}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </BodyPage>
   )
 }
