fe2b5e155c3aa16690a5c0a391f0cd868ec0cbbd
myl7 <myl@myl.moe>
2021-01-14 08:34:16 +0800

    New privacy policy page with BodyPage

--- src/pages/PrivacyPolicyPage.jsx
+++ src/pages/PrivacyPolicyPage.jsx
@@ -1,42 +1,16 @@
 import React from 'react'
-import {CardContent, Divider, makeStyles, Typography} from '@material-ui/core'
-import {Helmet} from 'react-helmet'
+import BodyPage from './templates/BodyPage'
 import Markdown from 'markdown-to-jsx'
-import Header from '../components/Header'
-import ContentCard from '../components/ContentCard'
-import WideDivider from '../components/WideDivider'
-import Footer from '../components/Footer'
-import privacyPolicyText from '../assets/privacyPolicyText.md'
 import {md2jsxOptions} from '../utils/md2jsx'
-
-const useStyles = makeStyles(theme => ({
-  content: {
-    backgroundColor: theme.palette.background.main
-  }
-}))
+import privacyPolicyText from '../assets/privacyPolicyText.md'
 
 export default () => {
-  const classes = useStyles()
-
   return (
-    <div>
-      <Helmet>
-        <title>Privacy policy | mylmoe</title>
-        <meta name={'description'} content={'mylmoe privacy policy page'} />
-        <link rel="canonical" href="https://myl.moe/pages/privacy-policy" />
-      </Helmet>
-      <Header />
-      <Divider />
-      <ContentCard className={classes.content}>
-        <CardContent>
-          <Typography variant={'h4'} component={'h1'}>
-            Privacy policy
-          </Typography>
-          <WideDivider />
-          <Markdown options={md2jsxOptions}>{privacyPolicyText}</Markdown>
-        </CardContent>
-      </ContentCard>
-      <Footer />
-    </div>
+    <BodyPage title={'Privacy Policy'} description={
+      'As a static and serverless website, This website does not collect any information on its own.\n' +
+      'However, external services that this website depends on may collect additional information.'
+    } subheader={'Updated on 2021-01-14'} path={'/pages/privacy-policy'}>
+      <Markdown options={md2jsxOptions}>{privacyPolicyText}</Markdown>
+    </BodyPage>
   )
 }
