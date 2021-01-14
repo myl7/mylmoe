ca4444849d6656b71fd065e9402e63ffc0831360
myl7 <myl@myl.moe>
2021-01-14 17:41:13 +0800

    Add fab to go top

--- src/pages/templates/BodyPage.jsx
+++ src/pages/templates/BodyPage.jsx
@@ -3,9 +3,26 @@ import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import HtmlHead from '../../components/HtmlHead'
 import BodyCard from '../../components/BodyCard'
+import {Fab, makeStyles} from '@material-ui/core'
+import {ArrowUpward as ArrowUpwardIcon} from '@material-ui/icons'
+import {animateScroll} from 'react-scroll'
+
+const useStyles = makeStyles({
+  fab: {
+    position: 'fixed',
+    bottom: '2em',
+    right: '2em'
+  }
+})
 
 export default props => {
   const {title, description, subheader, path, children, card = true} = props
+
+  const classes = useStyles()
+
+  const handleGoTopClick = () => {
+    animateScroll.scrollToTop()
+  }
 
   return (
     <div>
@@ -16,6 +33,9 @@ export default props => {
           {children}
         </BodyCard>
       ) : children}
+      <Fab aria-label={'go top'} className={classes.fab} color={'primary'} onClick={handleGoTopClick}>
+        <ArrowUpwardIcon />
+      </Fab>
       <Footer />
     </div>
   )
