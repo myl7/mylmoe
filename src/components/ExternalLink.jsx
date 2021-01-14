438820e2ccc7d83868d4c3bc180d8a8cb7ef0285
myl7 <myl@myl.moe>
2021-01-14 16:35:24 +0800

    Remove permanent underline of links

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,14 +1,6 @@
 import React, {forwardRef} from 'react'
-import {Link, makeStyles} from '@material-ui/core'
-
-const useStyles = makeStyles({
-  link: {
-    textDecoration: 'underline'
-  }
-})
+import {Link} from '@material-ui/core'
 
 export default forwardRef((props, ref) => {
-  const classes = useStyles()
-
-  return (<Link className={classes.link} target={'_blank'} rel={'noopener'} {...props} ref={ref} />)
+  return (<Link target={'_blank'} rel={'noopener'} {...props} ref={ref} />)
 })
--- src/components/RouterLink.jsx
+++ src/components/RouterLink.jsx
@@ -1,15 +1,7 @@
 import React, {forwardRef} from 'react'
-import {Link, makeStyles} from '@material-ui/core'
+import {Link} from '@material-ui/core'
 import {Link as RouterLink} from 'react-router-dom'
 
-const useStyles = makeStyles({
-  link: {
-    textDecoration: 'underline'
-  }
-})
-
 export default forwardRef((props, ref) => {
-  const classes = useStyles()
-
-  return (<Link className={classes.link} component={RouterLink} {...props} ref={ref} />)
+  return (<Link component={RouterLink} {...props} ref={ref} />)
 })
