912041f92a43f4ce64c6abbcdb35fffd1512617d
myl7 <myl@myl.moe>
2021-01-14 17:01:59 +0800

    Fix arcaea page

--- src/pages/ArcaeaPage.jsx
+++ src/pages/ArcaeaPage.jsx
@@ -1,13 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import {CardContent, Divider, AppBar, Tab, Typography, Box, Grid} from '@material-ui/core'
+import {AppBar, Tab, Typography, Box, Grid} from '@material-ui/core'
 import {TabList, TabPanel, TabContext} from '@material-ui/lab'
-import {Helmet} from 'react-helmet'
 import ArcaeaApi, {levels} from '../apis/ArcaeaApi'
 import {formatDatetime} from '../utils/dayjs'
 import ArcaeaSong from '../components/ArcaeaSong'
-import Header from '../components/Header'
-import ContentCard from '../components/ContentCard'
-import WideDivider from '../components/WideDivider'
+import BodyPage from './templates/BodyPage'
 
 export default () => {
   const [data, setData] = useState({
@@ -30,49 +27,36 @@ export default () => {
 
   const {songs, userInfo} = data
   return (
-    <div>
-      <Helmet>
-        <title>Arcaea scores | mylmoe</title>
-        <meta name={'description'} content={'mylmoe arcaea page containing myl7 arcaea song scores'} />
-        <link rel="canonical" href="https://myl.moe/pages/arcaea" />
-      </Helmet>
-      <Header />
-      <Divider />
-      <ContentCard>
-        <CardContent>
-          <Typography variant={'h4'} component={'h1'}>
-            Arcaea scores
-          </Typography>
-          <WideDivider />
-          <Typography variant={'body1'}>
-            <Typography component={'span'} variant={'h5'}>
-              {userInfo.name}
-            </Typography>
-            {' '}
-            <Box component={'span'} fontWeight={'fontWeightLight'}>
-              code: {userInfo.user_code}
-            </Box>
-            {' '}| PTT: {userInfo.rating} | joinTime: {formatDatetime(userInfo.join_date)} |
-          </Typography>
-          <TabContext value={tabNum} style={{marginTop: '0.5em'}}>
-            <AppBar position={'sticky'}>
-              <TabList onChange={handleTabSwitch} centered>
-                {levels.map(l => {
-                  const label = /\d+/.exec(l)[0] + (l[l.length - 1] === 'p' ? '+' : '')
-                  return (<Tab label={label} value={l} key={l} />)
-                })}
-              </TabList>
-            </AppBar>
-            {levels.map(l => (
-              <TabPanel value={l} key={l}>
-                <Grid container spacing={2} justify={'center'}>
-                  {songs[l].map(song => <ArcaeaSong key={song.song_id} song={song} />)}
-                </Grid>
-              </TabPanel>
-            ))}
-          </TabContext>
-        </CardContent>
-      </ContentCard>
-    </div>
+    <BodyPage title={'Arcaea scores'}
+              description={'myl7\'s Arcaea scores. Feel free to use the template to display your own Arcaea scores.'}
+              path={'/pages/arcaea'}>
+      <Typography variant={'body1'}>
+        <Typography component={'span'} variant={'h5'}>
+          {userInfo.name}
+        </Typography>
+        {' '}
+        <Box component={'span'} fontWeight={'fontWeightLight'}>
+          code: {userInfo.user_code}
+        </Box>
+        {' '}| PTT: {userInfo.rating} | joinTime: {formatDatetime(userInfo.join_date)} |
+      </Typography>
+      <TabContext value={tabNum} style={{marginTop: '0.5em'}}>
+        <AppBar position={'sticky'}>
+          <TabList onChange={handleTabSwitch} centered>
+            {levels.map(l => {
+              const label = /\d+/.exec(l)[0] + (l[l.length - 1] === 'p' ? '+' : '')
+              return (<Tab label={label} value={l} key={l} />)
+            })}
+          </TabList>
+        </AppBar>
+        {levels.map(l => (
+          <TabPanel value={l} key={l}>
+            <Grid container spacing={2} justify={'center'}>
+              {songs[l].map(song => <ArcaeaSong key={song.song_id} song={song} />)}
+            </Grid>
+          </TabPanel>
+        ))}
+      </TabContext>
+    </BodyPage>
   )
 }
