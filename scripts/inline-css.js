3f1613f032c3fe49e731764be1d9b13513190383
myl7 <myl@myl.moe>
2021-01-15 21:50:24 +0800

    Inline more css

--- scripts/inline-css.js
+++ scripts/inline-css.js
@@ -1,6 +1,23 @@
 import fs from 'fs'
+import https from 'https'
 
-const css = fs.readFileSync('public/index.css').toString()
-let html = fs.readFileSync('dist/tmp/index.html').toString()
-html = html.replace('/*css*/', css)
-fs.writeFileSync('dist/tmp/index.html', html)
+let css = fs.readFileSync('public/index.css').toString()
+https.get('https://fonts.googleapis.com/css2?family=Noto+Sans+SC&family=Roboto:wght@300;400;500;700&family=Source+Code+Pro&display=swap', res => {
+  res.on('data', d => {
+    css += d.toString()
+    https.get('https://fonts.googleapis.com/icon?family=Material+Icons', res => {
+      res.on('data', d => {
+        css += d.toString()
+        https.get('https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/styles/atom-one-dark.min.css', res => {
+          res.on('data', d => {
+            css += d.toString()
+
+            let html = fs.readFileSync('dist/tmp/index.html').toString()
+            html = html.replace('/*css*/', css)
+            fs.writeFileSync('dist/tmp/index.html', html)
+          })
+        }).end()
+      })
+    }).end()
+  })
+}).end()
