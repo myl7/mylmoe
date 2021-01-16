ab7dc159c9e25d7bdb260667ea3f7f9ddc12f4ed
myl7 <myl@myl.moe>
2021-01-16 09:15:37 +0800

    New brotli api

--- src/apis/BrotliEncApi.js
+++ src/apis/BrotliEncApi.js
@@ -1,10 +1,6 @@
-const subKey = '5bb30b9bf9ba47aa8418e25c3ae4ef51'
-
 export default class BrotliEncApi {
   async enc(src) {
-    const url = new URL('https://mlapis.azure-api.net/brotli-enc/enc')
-    url.search = new URLSearchParams({'subscription-key': subKey}).toString()
-    const res = await fetch(url.toString(), {
+    const res = await fetch('https://c.azure-api.net/brotli/enc', {
       method: 'POST',
       body: src.buffer
     })
