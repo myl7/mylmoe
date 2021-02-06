ac8d9fabb5b0f828330877d8f9e74c0c8209a32c
myl7 <myl@myl.moe>
2021-02-07 02:38:24 +0800

    Update package.json, add material-ui, add fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import 'normalize.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/source-code-pro/400.css'
 import './layout.css'
+import {Card} from '@material-ui/core'
 
 const Layout = props => {
   const {children} = props
 
   return (
     <div>
-      {children}
+      <Card variant="outlined" style={{margin: '5em 1em 0 1em'}}>
+        {children}
+      </Card>
     </div>
   )
 }
