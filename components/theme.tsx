8b798da20e2cff0b04a98472825ce99888019ce5
myl7 <myl@myl.moe>
2021-06-06 21:12:07 +0800

    Perform theme wrapper

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,7 +1,8 @@
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core'
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {State} from '../redux/state'
+import {themeInitAction} from '../redux/theme'
 
 const theme = (dark: boolean) => createMuiTheme({
   typography: {
@@ -16,6 +17,16 @@ const theme = (dark: boolean) => createMuiTheme({
 
 const Theme: React.FC = ({children}) => {
   const dark = useSelector((state: State) => state.theme.dark)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const key = 'mylmoe.dark'
+    const darkStore = localStorage.getItem(key)
+    const dark = (darkStore === null ? false : Boolean(darkStore))
+    themeEffect(dark)
+    dispatch(themeInitAction(dark))
+  })
 
   return (
     <MuiThemeProvider theme={theme(dark)}>
