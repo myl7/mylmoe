2bb076098f2721a5c931b8ad559dce1ed5ef7810
myl7 <myl@myl.moe>
2021-06-07 00:51:12 +0800

    Adapt head component

--- packages/mylmoe/pages/index.tsx
+++ packages/mylmoe/pages/index.tsx
@@ -1,8 +1,16 @@
 import Comment from '../components/comment'
+import Head from '../components/head'
 
 const Index = () => {
   return (
-    <Comment />
+    <>
+      <Head
+        title={'Index & Post List'}
+        description={'All blog posts & some probably useful utilities made by myl7'}
+        path={'/'}
+      />
+      <Comment />
+    </>
   )
 }
 
