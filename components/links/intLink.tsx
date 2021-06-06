950783126e4ff089ffd483fa4b735d5d0a65653c
myl7 <myl@myl.moe>
2021-06-06 23:12:04 +0800

    Fix links

--- components/links/intLink.tsx
+++ components/links/intLink.tsx
@@ -1,9 +1,21 @@
-import React, {forwardRef} from 'react'
 import {Link, LinkProps} from '@material-ui/core'
 import {default as NextLink, LinkProps as NextLinkProps} from 'next/link'
+import React, {forwardRef} from 'react'
 
-const IntLink = forwardRef<HTMLLinkElement, LinkProps&NextLinkProps>((props, ref) => {
-  return <Link component={NextLink} ref={ref} {...props} />
+const RefLink = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+  return <Link ref={ref} {...props} />
 })
+
+const IntLink: React.FC<NextLinkProps&{linkProps?: LinkProps}> = props => {
+  const {children, linkProps, passHref, ...others} = props
+
+  return (
+    <NextLink {...others} passHref>
+      <RefLink {...linkProps}>
+        {children}
+      </RefLink>
+    </NextLink>
+  )
+}
 
 export default IntLink
