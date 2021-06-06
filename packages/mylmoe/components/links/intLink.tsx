7ab96c7eb027ef8b7e5659f06b6a2d17cdf076f0
myl7 <myl@myl.moe>
2021-06-07 00:52:55 +0800

    Update FC importing

--- packages/mylmoe/components/links/intLink.tsx
+++ packages/mylmoe/components/links/intLink.tsx
@@ -1,12 +1,12 @@
 import {Link, LinkProps} from '@material-ui/core'
 import {default as NextLink, LinkProps as NextLinkProps} from 'next/link'
-import React, {forwardRef} from 'react'
+import {FC, forwardRef} from 'react'
 
 const RefLink = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
   return <Link ref={ref} {...props} />
 })
 
-const IntLink: React.FC<NextLinkProps&{linkProps?: LinkProps}> = props => {
+const IntLink: FC<NextLinkProps&{linkProps?: LinkProps}> = props => {
   const {children, linkProps, passHref, ...others} = props
 
   return (
