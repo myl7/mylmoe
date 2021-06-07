909c20c2c53d91cdefbd20d1bd1afda4eb5d2072
myl7 <myl@myl.moe>
2021-06-08 07:01:16 +0800

    Fix redux with adding next-redux-wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,29 @@
 import 'normalize.css'
-
 import '../styles.css'
-
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import '@fontsource/source-code-pro/400.css'
-
 import type {AppProps} from 'next/app'
-import {Provider} from 'react-redux'
-import store from '../redux/store'
 import Theme from '../components/theme'
 import Footer from '../components/footer'
 import {Card} from '@material-ui/core'
 import FloatAction from '../components/floatAction'
 import Header from '../components/header'
+import {storeWrapper} from '../redux/store'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <Provider store={store}>
-      <Theme>
-        <Header />
-        <Card variant="outlined" style={{margin: '5em 1em 0 1em'}} component="main">
-          <Component {...pageProps} />
-        </Card>
-        <Footer />
-        <FloatAction />
-      </Theme>
-    </Provider>
+    <Theme>
+      <Header />
+      <Card variant="outlined" style={{margin: '5em 1em 0 1em'}} component="main">
+        <Component {...pageProps} />
+      </Card>
+      <Footer />
+      <FloatAction />
+    </Theme>
   )
 }
 
-export default MyApp
+export default storeWrapper.withRedux(MyApp)
