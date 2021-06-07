75ab8f382e9dcdc27312fc9e5fcf41ac0639fc5a
myl7 <myl@myl.moe>
2021-06-08 07:01:16 +0800

    Fix redux with adding next-redux-wrapper

--- packages/mylmoe/redux/store.ts
+++ packages/mylmoe/redux/store.ts
@@ -1,10 +1,17 @@
-import {createStore} from 'redux'
+import {AnyAction, createStore, Store} from 'redux'
 import {ThemeAction, themeReducer} from './theme'
 import {initState, State} from './state'
+import {createWrapper, HYDRATE} from 'next-redux-wrapper'
+import produce from 'immer'
 
-type Action = ThemeAction
+type Action = ThemeAction|AnyAction
 
 const reducer = (state: State = initState, action: Action) => {
+  if (action.type === HYDRATE) {
+    return produce(state, state => (
+      {...state, ...(action.payload as State)}
+    ))
+  }
   const domain = action.type.substring(0, action.type.indexOf('.'))
   switch (domain) {
     case 'theme':
@@ -14,10 +21,10 @@ const reducer = (state: State = initState, action: Action) => {
   }
 }
 
-const store = createStore(
+const makeStore = () => createStore(
   reducer, initState
   // @ts-ignore
   // typeof window !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : undefined
 )
 
-export default store
+export const storeWrapper = createWrapper<Store<State>>(makeStore)
