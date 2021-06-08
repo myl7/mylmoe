27c20558ac65fb5f822eb00bead44ca6b362d90d
myl7 <myl@myl.moe>
2021-06-08 21:09:27 +0800

    Add rss script

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-#esbuild scripts/rss.ts --bundle --outfile=scripts/rss.js
-#node scripts/rss.js
+esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/rss.js
+node scripts/rss.js
 esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/sitemap.js
 node scripts/sitemap.js
