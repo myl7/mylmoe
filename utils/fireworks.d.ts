a17b86f7ff964d564a728b16f702bd351a165417
myl7 <myl@myl.moe>
2021-06-08 22:49:41 +0800

    Add fireworks

---
+++ utils/fireworks.d.ts
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function tap(): keyof DocumentEventMap
+
+export function handleResize(elem: HTMLCanvasElement): () => void
+
+export function handleTap(filter: (e: React.MouseEvent) => boolean, elem: HTMLCanvasElement): (e: React.MouseEvent) => void
