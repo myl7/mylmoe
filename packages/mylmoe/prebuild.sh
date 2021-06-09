5b860cffcfe901e837dfbde0a5452a0565fcd1be
myl7 <myl@myl.moe>
2021-06-09 18:39:03 +0800

    Fix esbuild react importing in scripts

--- packages/mylmoe/prebuild.sh
+++ packages/mylmoe/prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/rss.js
+esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/rss.js --inject:scripts/react-shim.ts
 node scripts/rss.js
-esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/sitemap.js
+esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/sitemap.js --inject:scripts/react-shim.ts
 node scripts/sitemap.js
