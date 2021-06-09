796d2e37713a2135255b9e2b2b18e754e4d05999
myl7 <myl@myl.moe>
2021-06-09 23:18:12 +0800

    Update header component positions and functions

--- components/header/search.tsx
+++ components/header/search.tsx
@@ -1,20 +1,14 @@
-import React, {FC, useRef} from 'react'
-import {Grid, GridProps, IconButton, TextField, TextFieldProps} from '@material-ui/core'
+import React, {useRef} from 'react'
+import {Grid, IconButton, TextField} from '@material-ui/core'
 import {Search as SearchIcon} from '@material-ui/icons'
 import site from '../../config/site'
-
-export interface SearchProps extends GridProps {
-  textFieldProps?: TextFieldProps
-}
 
 const searchUrl = (q: string) => {
   const params = new URLSearchParams([['q', q], ['as_sitesearch', site.url], ['ncr', '1']])
   return 'https://www.google.com/search?' + params.toString()
 }
 
-const Search: FC<SearchProps> = props => {
-  const {textFieldProps, ...others} = props
-
+const Search = () => {
   const ref = useRef<HTMLInputElement>()
 
   const handleEnter = (e: React.KeyboardEvent) => {
@@ -30,17 +24,19 @@ const Search: FC<SearchProps> = props => {
   }
 
   return (
-    <Grid container spacing={1} {...others} alignItems="center">
-      <Grid item>
-        <IconButton onClick={handleClick} aria-label="Search">
-          <SearchIcon />
-        </IconButton>
+    <div>
+      <Grid container spacing={1} alignItems="center">
+        <Grid item>
+          <IconButton onClick={handleClick} aria-label="Search">
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <TextField type="search" label="Search..." variant="outlined" size="small" onKeyDown={handleEnter}
+                     inputRef={ref} />
+        </Grid>
       </Grid>
-      <Grid item>
-        <TextField type="search" label="Search..." variant="outlined" size="small"
-                   onKeyDown={handleEnter} inputRef={ref} {...textFieldProps} />
-      </Grid>
-    </Grid>
+    </div>
   )
 }
 
