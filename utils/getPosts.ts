3b0b11a53fa44b914c165edf5cfe28172bead41b
myl7 <myl@myl.moe>
2021-06-11 03:50:23 +0800

    Use func memo for getPosts

--- utils/getPosts.ts
+++ utils/getPosts.ts
@@ -1,21 +1,16 @@
 import path from 'path'
 import fs from 'fs'
 import parse from '../remark/parse'
-import {PostInfo} from '../remark/post'
+import lodash from 'lodash'
 
-const cache = {} as {[key: string]: PostInfo[]|null}
-
-const getPosts = (folder: string = 'posts') => {
-  if (!cache[folder]) {
-    const postDir = path.join(process.cwd(), 'config', folder)
-    const names = fs.readdirSync(postDir)
-    cache[folder] = names.filter(name => name.endsWith('.md')).map(name => {
-      const filePath = path.join(postDir, name)
-      const content = fs.readFileSync(filePath).toString()
-      return parse(name, content)
-    })
-  }
-  return cache[folder]!
-}
+const getPosts = lodash.memoize((folder: string = 'posts') => {
+  const postDir = path.join(process.cwd(), 'config', folder)
+  const names = fs.readdirSync(postDir)
+  return names.filter(name => name.endsWith('.md')).map(name => {
+    const filePath = path.join(postDir, name)
+    const content = fs.readFileSync(filePath).toString()
+    return parse(name, content)
+  })
+})
 
 export default getPosts
