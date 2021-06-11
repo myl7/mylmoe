5cdd86d6fe76f3b5b3ab98e786203a6fa5aa4573
myl7 <myl@myl.moe>
2021-06-11 09:21:14 +0800

    Update heading anchor. Refactor to react to hast.

--- remark/react2hast.tsx
+++ remark/react2hast.tsx
@@ -1,17 +1,14 @@
 import {renderToString} from 'react-dom/server'
 import unified from 'unified'
 import rehypeParse from 'rehype-parse'
-import ExtLinkSign from '../components/links/extLinkSign'
 import type {Root, Element} from 'hast'
 
-const extLinkSign = () => {
-  const html = renderToString(
-    <ExtLinkSign />
-  )
+const react2hast = (elem: JSX.Element) => {
+  const html = renderToString(elem)
   const res = unified()
     .use(rehypeParse, {fragment: true})
     .parse(html) as Root
   return res['children']![0]! as Element
 }
 
-export default extLinkSign()
+export default react2hast
