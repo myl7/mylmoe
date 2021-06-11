df4416703b422d341b199c5d900dd41f3db23145
myl7 <myl@myl.moe>
2021-06-11 08:16:50 +0800

    Fix inline code style

--- packages/mylmoe/remark/fixStyles.ts
+++ packages/mylmoe/remark/fixStyles.ts
@@ -2,18 +2,10 @@ const fixStyles = (elem: HTMLDivElement) => {
   if (!elem) {
     return
   }
-  fixInlineCode(elem)
   fixRespImage(elem)
 }
 
-export const fixInlineCode = (elem: HTMLDivElement) => {
-  const classes = ['inline-code', 'MuiPaper-rounded']
-  elem.querySelectorAll(':not(pre) > code').forEach(inlineCode => {
-    inlineCode.classList.add(...classes)
-  })
-}
-
-export const fixRespImage = (elem: HTMLDivElement) => {
+const fixRespImage = (elem: HTMLDivElement) => {
   elem.querySelectorAll('img:not(.not-resp-image):not(.resp-image-vert)').forEach(respImage => {
     respImage.classList.add('resp-image')
   })
