8ec604b1775d0363e58a98c8c3ea88fbbd19b4a4
myl7 <myl@myl.moe>
2021-06-12 00:12:35 +0800

    Update api domain

--- packages/mylmoe/api/brotli.ts
+++ packages/mylmoe/api/brotli.ts
@@ -1,7 +1,7 @@
 import {fromUint8Array, toUint8Array} from 'js-base64'
 
 export const brotliEnc = async (arr: Uint8Array) => {
-  const res = await fetch('https://1g067nz0ob.execute-api.us-west-2.amazonaws.com/brotli-enc', {
+  const res = await fetch('https://api.myl.moe/brotli-enc', {
     method: 'POST',
     body: fromUint8Array(arr)
   })
