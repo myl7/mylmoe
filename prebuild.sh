73c7abeb38556bb789d66ed57cc3f8564c826a06
myl7 <myl@myl.moe>
2021-06-13 05:30:50 +0800

    Change prebuild build output to build folder

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/rss.js --inject:scripts/react-shim.ts
-node scripts/rss.js
-esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/sitemap.js --inject:scripts/react-shim.ts
-node scripts/sitemap.js
+esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/build/rss.js --inject:scripts/react-shim.ts
+node scripts/build/rss.js
+esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/build/sitemap.js --inject:scripts/react-shim.ts
+node scripts/build/sitemap.js
