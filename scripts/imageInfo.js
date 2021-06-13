e93d7199f95dd55645686148bbd0ad9ca053464c
myl7 <myl@myl.moe>
2021-06-13 10:07:49 +0800

    Fix to store image info in extra file or env

---
+++ scripts/imageInfo.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const glob = require('glob')
+const imageSize = require('image-size')
+const fs = require('fs')
+
+const p = path.join(process.cwd(), 's3', 'images', '**', '*')
+let info = {}
+glob.sync(p).filter(p => {
+  const ext = path.extname(p)
+  const name = path.basename(p, ext)
+  return ext && name !== 'README' && name !== 'LICENSE'
+}).forEach(p => {
+  const {width, height} = imageSize(p)
+  const k = path.relative(process.cwd(), p)
+  info[k] = {width, height}
+})
+const f = path.join(process.cwd(), 'config', 'images.json')
+fs.writeFileSync(f, JSON.stringify(info))
