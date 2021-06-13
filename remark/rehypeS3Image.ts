448f44878de60e2a8d3195a0ccdc80c01601e115
myl7 <myl@myl.moe>
2021-06-13 09:08:34 +0800

    Update unified image processing

--- remark/rehypeS3Image.ts
+++ remark/rehypeS3Image.ts
@@ -3,17 +3,68 @@ import visit, {Visitor} from 'unist-util-visit'
 import type {Element} from 'hast'
 // @ts-ignore
 import isElement from 'hast-util-is-element'
+import h from 'hastscript'
+import path from 'path'
+import fs from 'fs'
+import imageSize from 'image-size'
 
 export interface RehypeS3ImageSetting {
   baseUrl: string
 }
 
+const breakPoints = [200, 400, 600, 800, 1000]
+
 const rehypeS3Image: Plugin<RehypeS3ImageSetting[]> = setting => {
   const {baseUrl} = setting
 
-  const visitor: Visitor<Element> = (node, _i, _parent) => {
+  const visitor: Visitor<Element> = (node, i, parent) => {
     const src = node.properties!['src'] as string
-    node.properties!['src'] = src.replace(/^\.\.\/\.\.\/s3\/images/, baseUrl)
+    const prefix = /^\.\.\/\.\.\/s3\/images/
+    if (!prefix.test(src)) {
+      return
+    }
+    const url = src.replace(prefix, baseUrl)
+    node.properties!['src'] = url
+
+    let s = path.join(process.cwd(), src.replace(/^\.\.\/\.\.\//, ''))
+    const {width: ws, height: hs} = imageSize(s)
+    const ext = path.extname(s)
+    const name = path.basename(s, ext)
+    let vert = false
+    let p = path.join(path.dirname(s), name + `_h${breakPoints[0]}.webp`)
+    if (fs.existsSync(p)) {
+      vert = true
+    }
+
+    const pic = h('picture')
+    for (let i = 0; i < breakPoints.length; i++) {
+      const point = breakPoints[i]!
+      let p = path.join(path.dirname(s), name + `_${vert ? 'h' : 'w'}${point}.webp`)
+      if (fs.existsSync(p)) {
+        const {width, height} = imageSize(p)
+        const sizes = `${width}px,${height}px`
+        if (i < breakPoints.length - 1) {
+          const media = `(max-${vert ? 'height' : 'width'}:${breakPoints[i + 1]! + (vert ? 0 : 100)}px)`
+          const srcset = p.replace(/^.*s3\/images/, baseUrl)
+          pic.children.push(h('source', {media, srcset, sizes}))
+        } else {
+          const srcset = p.replace(/^.*s3\/images/, baseUrl)
+          pic.children.push(h('source', {srcset, sizes}))
+        }
+      } else {
+        let p = path.join(path.dirname(s), name + '.webp')
+        if (fs.existsSync(p)) {
+          const {width, height} = imageSize(p)
+          const sizes = `${width}px,${height}px`
+          const srcset = p.replace(/^.*s3\/images/, baseUrl)
+          pic.children.push(h('source', {srcset, sizes}))
+        }
+        break
+      }
+    }
+    node.properties!['sizes'] = `${ws}px,${hs}px`
+    pic.children.push(node)
+    parent!.children[i] = h('a', {target: '_blank', href: url}, [pic])
   }
 
   return tree => visit(tree, isElement.convert('img'), visitor)
