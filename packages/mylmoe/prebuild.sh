3ebe66383d08bd3e2fd4c4e1806ee1cb7dffcba1
myl7 <myl@myl.moe>
2021-06-26 17:01:04 +0800

    Add RSS3

--- packages/mylmoe/prebuild.sh
+++ packages/mylmoe/prebuild.sh
@@ -5,3 +5,5 @@ esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/build/rss.js -
 node scripts/build/rss.js
 esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/build/sitemap.js --inject:scripts/react-shim.ts
 node scripts/build/sitemap.js
+esbuild scripts/rss3.ts --bundle --platform=node --outfile=scripts/build/rss3.js --inject:scripts/react-shim.ts
+node scripts/build/rss3.js
