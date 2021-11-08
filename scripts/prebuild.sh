5810db3181f13bb4b1ac4709a2a0b93ff87fb376
myl7 <myl@myl.moe>
2021-11-08 15:05:54 +0800

    refactor: Move shin to misc folder

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 mkdir -p data
-esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/build/rss.js --inject:scripts/react-shim.ts
+esbuild scripts/rss.ts --bundle --platform=node --outfile=scripts/build/rss.js --inject:scripts/misc/react-shim.ts
 node scripts/build/rss.js
-esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/build/sitemap.js --inject:scripts/react-shim.ts
+esbuild scripts/sitemap.ts --bundle --platform=node --outfile=scripts/build/sitemap.js --inject:scripts/misc/react-shim.ts
 node scripts/build/sitemap.js
-esbuild scripts/tagrel.ts --bundle --platform=node --outfile=scripts/build/tagrel.js --inject:scripts/react-shim.ts
+esbuild scripts/tagrel.ts --bundle --platform=node --outfile=scripts/build/tagrel.js --inject:scripts/misc/react-shim.ts
 node scripts/build/tagrel.js
