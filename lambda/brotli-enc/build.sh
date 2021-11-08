c46379e1f3615307071ddfd57fa7507c76f09930
myl7 <myl@myl.moe>
2021-11-08 15:09:32 +0800

    refactor: Remove unused lambda files

--- lambda/brotli-enc/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-rm -rf build build.zip
-mkdir -p build
-cp lambda_function.py build/
-pip install --target=build -r requirements.txt
-(cd build && zip -r ../build.zip .)
--- lambda/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-aws lambda update-function-code --function-name $1 --zip-file fileb://build.zip
