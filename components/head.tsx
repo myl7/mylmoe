2a4afabbb27265d34a43cca3991519284f85bd33
myl7 <myl@myl.moe>
2021-12-02 05:52:33 +0800

    feat: Update head and inline favicon

--- components/head.tsx
+++ components/head.tsx
@@ -1,25 +1,25 @@
 import {FC} from 'react'
 import site from '../content/site'
 import {default as NextHead} from 'next/head'
-import Icon from './icon'
 
 export interface HeadProps {
-  title: string,
-  description: string,
+  title?: string,
+  description?: string,
   path: string
 }
 
 const Head: FC<HeadProps> = props => {
-  const {title, description: d, path, children} = props
-
-  const description = d ? d : '...No description currently'
+  const {title: t, description: d, path, children} = props
+  const title = t ? `${t} | ${site.title}` : site.title
+  const description = d ? `${d} | ${site.title}: ${site.description}` : (
+    t ? `${site.title}: ${site.description}` : site.description
+  )
 
   return (
     <NextHead>
-      <title>{title} | {site.title}</title>
-      <meta name={'description'} content={`${description} | ${site.title}: ${site.description}`} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <link rel="canonical" href={site.url + path} />
-      <Icon />
       {children}
     </NextHead>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=Roboto:wght@300;400;500;700&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
