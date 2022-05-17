480b52837f06226be58cde17ee0fb4064377eaca
myl7 <myl@myl.moe>
2022-05-17 19:56:19 +0800

    Update MDX default style for links

--- remark/mdxComponents.tsx
+++ remark/mdxComponents.tsx
@@ -3,12 +3,19 @@
 
 import Image from 'next/image'
 import { MDXComponents } from 'mdx/types'
+import ExtLink from '../components/links/extLink'
 
 // Only for ![]() syntax
 export const MdxImage = (props: { src?: string; alt?: string }) => <Image src={props.src!} alt={props.alt!} />
 
+// []() should only be used for external links
+export const MdxExtLink = (props: { href?: string; children?: React.ReactNode }) => (
+  <ExtLink href={props.href!}>{props.children}</ExtLink>
+)
+
 const mdxComponents: MDXComponents = {
   img: MdxImage,
+  a: MdxExtLink,
 }
 
 export default mdxComponents
