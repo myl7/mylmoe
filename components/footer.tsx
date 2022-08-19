14da2f2a95692acad2837dab9b43bbe563da6c0d
myl7 <myl@myl.moe>
2022-08-19 08:35:23 +0800

    Add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,53 @@
+// Copyright (C) 2022 myl7
+// SPDX-License-Identifier: Apache-2.0
+
+import { Box, Flex, HStack, Icon, Link, Spacer, useColorModeValue } from '@chakra-ui/react'
+import { FaCreativeCommons, FaCreativeCommonsBy, FaCreativeCommonsSa } from 'react-icons/fa'
+
 export default function Footer() {
-  return <footer></footer>
+  const colors = {
+    lineColor: useColorModeValue('blackAlpha.800', 'whiteAlpha.800'),
+    linkColor: useColorModeValue('blue.500', 'blue.300'),
+  }
+
+  return (
+    <Flex
+      direction="column"
+      as="footer"
+      fontSize="sm"
+      px={4}
+      py={2}
+      borderWidth={1.5}
+      borderColor={colors.lineColor}
+      w="100%"
+    >
+      <Box>Copyright (C) 2020, 2021, 2022 myl7</Box>
+      <Box>
+        Posts are licensed under <Icon as={FaCreativeCommons} w={4} h={4} />
+        <Icon as={FaCreativeCommonsBy} w={4} h={4} />
+        <Icon as={FaCreativeCommonsSa} w={4} h={4} />{' '}
+        <Link
+          href="https://creativecommons.org/licenses/by-sa/4.0/"
+          rel="license noopener noreferrer"
+          target="_blank"
+          textColor={colors.linkColor}
+        >
+          <Box as="span">CC BY-SA 4.0</Box>
+        </Link>{' '}
+        by default unless otherwise explicitly stated. Posts with different licenses will contain a section{' '}
+        <Box as="span" fontStyle="italic">
+          License
+        </Box>{' '}
+        to indicate their respective licenses.
+      </Box>
+      <Box>
+        Website source code is available on{' '}
+        <Link textColor={colors.linkColor} href="https://github.com/myl7/mylmoe">
+          myl7/mylmoe@GitHub
+        </Link>
+      </Box>
+      <Spacer />
+      <Box></Box>
+    </Flex>
+  )
 }
