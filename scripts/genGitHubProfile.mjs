7d99e9c7a40a7aaa83cff947d155963c784685aa
myl7 <myl@myl.moe>
2022-10-02 15:51:13 +0800

    Format code

--- scripts/genGitHubProfile.mjs
+++ scripts/genGitHubProfile.mjs
@@ -12,10 +12,14 @@ const fmEnd = lines.findIndex((line) => line == '---')
 lines.splice(0, fmEnd + 1)
 const hStart = lines.findIndex((line) => line.startsWith('<!--') && line.endsWith('-->'))
 const hEnd = lines.slice(hStart + 1).findIndex((line) => line.startsWith('<!--') && line.endsWith('-->')) + hStart + 1
-lines.splice(hEnd + 1, 0, ...[
-  '',
-  '# Hi, I am myl7!',
-  '',
-  "![myl7's GitHub Stats](https://github-readme-stats.vercel.app/api?username=myl7&count_private=true&theme=gruvbox&show_icons=true)",
-])
+lines.splice(
+  hEnd + 1,
+  0,
+  ...[
+    '',
+    '# Hi, I am myl7!',
+    '',
+    "![myl7's GitHub Stats](https://github-readme-stats.vercel.app/api?username=myl7&count_private=true&theme=gruvbox&show_icons=true)",
+  ]
+)
 process.stdout.write(lines.join('\n').trimStart())
