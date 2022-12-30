3471ae6b60015b7c50672ceaf178f3208ddc9e6b
myl7 <myl@myl.moe>
2022-12-30 20:58:19 +0800

    Update home page post list display

--- app/page.tsx
+++ app/page.tsx
@@ -19,43 +19,71 @@ export default async function Page() {
         <h2 className="text-xl">I am myl7:</h2>
         <MDXRemote {...mdxSrc} />
       </section>
-      <section className="grid grid-cols-1 gap-4 font-serif md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {Object.entries(postMetas)
-          .sort(([slug1, meta1], [slug2, meta2]) => {
-            const updCmp = -meta1.updDate.localeCompare(meta2.updDate)
-            if (updCmp != 0) return updCmp
-            const pubCmp = -meta1.pubDate.localeCompare(meta2.pubDate)
-            if (pubCmp != 0) return pubCmp
-            return slug1.localeCompare(slug2)
-          })
-          .map(([slug, meta]) => (
-            <div key={slug}>
-              <a href={`/posts/${slug}`} className="group">
-                <article className="flex flex-col gap-1 rounded border-2 border-bg-l4 bg-bg-l1 p-2 ring-blue group-hover:ring-2 dark:border-bg-d4 dark:bg-bg-d1">
-                  <h2 className="text-xl">{meta.title}</h2>
+      <section>
+        <details open>
+          <summary className="cursor-pointer rounded border-2 border-bg-l4 bg-bg-l1 p-2 marker:text-xl dark:border-bg-d4 dark:bg-bg-d1">
+            <h2 className="inline-block text-xl">Subprojects in the website:</h2>
+          </summary>
+          <div className="grid grid-cols-1 gap-4 pt-4 font-serif md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div>
+              <a href="/brotli" className="group block">
+                <article className="flex flex-col gap-1 rounded border-2 border-t-[6px] border-bg-l4 bg-bg-l1 p-2 pt-1 ring-blue group-hover:ring-2 dark:border-bg-d4 dark:bg-bg-d1">
+                  <h2 className="text-xl">Brotli encode/decode tool</h2>
                   <hr className="border-bg-l4 dark:border-bg-d4" />
-                  <p className="text-sm">
-                    {meta.pubDate == meta.updDate
-                      ? `updated & published on ${meta.updDate}`
-                      : `updated on ${meta.updDate} & published on ${meta.pubDate}`}
+                  <p>
+                    Encode/decode (a.k.a. compress/decompress) data in Brotli format locally in browser. All processing
+                    is literally done locally with WebAssembly to keep data safe.
                   </p>
-                  <hr className="border-bg-l4 dark:border-bg-d4" />
-                  <p>{meta.abstract}</p>
-                  <hr className="border-bg-l4 dark:border-bg-d4" />
-                  <div className="flex w-full flex-wrap gap-1 py-0.5">
-                    {meta.tags.map((tag) => (
-                      <span
-                        className="rounded border border-bg-l4 bg-bg-l2 px-1 dark:border-bg-d4 dark:bg-bg-d2"
-                        key={tag}
-                      >
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
                 </article>
               </a>
             </div>
-          ))}
+          </div>
+        </details>
+      </section>
+      <section>
+        <details open>
+          <summary className="cursor-pointer rounded border-2 border-bg-l4 bg-bg-l1 p-2 marker:text-xl dark:border-bg-d4 dark:bg-bg-d1">
+            <h2 className="inline-block text-xl">Blog posts:</h2>
+          </summary>
+          <div className="grid grid-cols-1 gap-4 pt-4 font-serif md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {Object.entries(postMetas)
+              .sort(([slug1, meta1], [slug2, meta2]) => {
+                const updCmp = -meta1.updDate.localeCompare(meta2.updDate)
+                if (updCmp != 0) return updCmp
+                const pubCmp = -meta1.pubDate.localeCompare(meta2.pubDate)
+                if (pubCmp != 0) return pubCmp
+                return slug1.localeCompare(slug2)
+              })
+              .map(([slug, meta]) => (
+                <div key={slug}>
+                  <a href={`/posts/${slug}`} className="group block">
+                    <article className="flex flex-col gap-1 rounded border-2 border-t-[6px] border-bg-l4 bg-bg-l1 p-2 pt-1 ring-blue group-hover:ring-2 dark:border-bg-d4 dark:bg-bg-d1">
+                      <h2 className="text-xl">{meta.title}</h2>
+                      <hr className="border-bg-l4 dark:border-bg-d4" />
+                      <p className="text-sm">
+                        {meta.pubDate == meta.updDate
+                          ? `updated & published on ${meta.updDate}`
+                          : `updated on ${meta.updDate} & published on ${meta.pubDate}`}
+                      </p>
+                      <hr className="border-bg-l4 dark:border-bg-d4" />
+                      <p>{meta.abstract}</p>
+                      <hr className="border-bg-l4 dark:border-bg-d4" />
+                      <div className="flex w-full flex-wrap gap-1 py-0.5">
+                        {meta.tags.map((tag) => (
+                          <span
+                            className="rounded border border-bg-l4 bg-bg-l2 px-1 dark:border-bg-d4 dark:bg-bg-d2"
+                            key={tag}
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    </article>
+                  </a>
+                </div>
+              ))}
+          </div>
+        </details>
       </section>
     </main>
   )
