de5c0e39709fea42825a3eeef436ebb4877beeb6
myl7 <myl@myl.moe>
2024-04-18 00:49:40 +0800

    Add state icon for collapsible

--- src/components/ProfileCollapsible.tsx
+++ src/components/ProfileCollapsible.tsx
@@ -1,15 +1,21 @@
-import { Link } from 'lucide-react'
+import React from 'react'
+import { Plus, Minus } from 'lucide-react'
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
-import type React from 'react'
 
 export default function ProfileCollapsible({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = React.useState(false)
+  const Action = open ? Minus : Plus
+
   return (
-    <Collapsible>
+    <Collapsible open={open} onOpenChange={setOpen}>
       <CollapsibleTrigger className="w-full">
         <div className="flex flex-col gap-1">
-          <h2 className="text-start text-xl">
-            About me <Link className="inline h-5 w-5" />
+          <h2 className="flex gap-4 text-xl">
+            <span className="text-start">About me</span>
+            <span>
+              <Action className="inline h-5 w-5" />
+            </span>
           </h2>
           <hr />
         </div>
