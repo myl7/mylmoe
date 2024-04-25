4bed9ed4df4946c292e0a5a186d658c1a235c3f1
myl7 <myl@myl.moe>
2024-04-25 22:54:31 +0800

    Add locale to post scheme

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,19 @@
 import { z, defineCollection } from 'astro:content'
 
+const LOCALE_REGEX = /^[a-z]+_[A-Z]+$/
+
 const postScheme = z.object({
   title: z.string(),
   pubDate: z.date(),
   updDate: z.date().optional(),
   unlisted: z.boolean().optional(),
   description: z.string(),
+  locale: z
+    .string()
+    .optional()
+    .refine((locale) => (locale ? LOCALE_REGEX.test(locale) : true), {
+      message: 'locale must be of the format `language_TERRITORY`',
+    }),
 })
 export type PostScheme = z.infer<typeof postScheme>
 const posts = defineCollection({
